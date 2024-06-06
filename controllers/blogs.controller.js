ec62bb41eb7c0b3f487e4e1de9d5a5fb2ed3fbc9
ikun <0215.miyatto@gmail.com>
2024-06-06 12:49:19 +0900

    notionとの接続

--- controllers/blogs.controller.js
+++ controllers/blogs.controller.js
@@ -1,25 +1,33 @@
-const blogs = [
-  {
-    id: 1,
-    title: "first title",
-    description: "first description"
-  },
-  {
-    id: 2,
-    title: "second title",
-    description: "second description"
-  },
-  {
-    id: 3,
-    title: "third title",
-    description: "third description"
-  },
-]
+const notion = require('../lib/notionAPI');
+
+const getPageMetaData = (blog) => {
+  const getTags = (tags) => {
+    const allTags = tags.map((tag) => {
+      return tag.name;
+    });
+    return allTags;
+  };
+  return {
+    title: blog.properties.Title.title[0].plain_text,
+    description: blog.properties.Description.rich_text[0].plain_text,
+    slug: blog.properties.Slug.rich_text[0].plain_text,
+    date: blog.properties.Date.date.start,
+    tags: getTags(blog.properties.Tags.multi_select),
+    image: blog.properties.Image.files[0],
+  };
+}
 
 // @GET /api/blogs
-exports.getAllBlogs = (req, res) => {
+exports.getAllBlogs = async (req, res) => {
   console.log('@GET /api/blogs getAllBlogs');
-  res.status(200).json(blogs);
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+  });
+  const blogs = response.results;
+  const metadatas = blogs.map((blog) => {
+    return getPageMetaData(blog);
+  })
+  res.status(200).json(metadatas);
 };
 
 // @GET /api/blogs/:blogId
