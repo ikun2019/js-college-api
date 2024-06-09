13909b1a6bc6a59399ddfcf0c69eeeb4c77fc8f9
ikun <0215.miyatto@gmail.com>
2024-06-10 06:05:14 +0900

    metadataの設定

--- controllers/blogs.controller.js
+++ controllers/blogs.controller.js
@@ -1,5 +1,7 @@
+const { NotionToMarkdown } = require('notion-to-md');
 const notion = require('../lib/notionAPI');
 
+// * メタ情報を取得するための関数
 const getPageMetaData = (blog) => {
   const getTags = (tags) => {
     const allTags = tags.map((tag) => {
@@ -8,32 +10,68 @@ const getPageMetaData = (blog) => {
     return allTags;
   };
   return {
-    title: blog.properties.Title.title[0].plain_text,
-    description: blog.properties.Description.rich_text[0].plain_text,
-    slug: blog.properties.Slug.rich_text[0].plain_text,
+    title: blog.properties.Title.title[0]?.plain_text || '',
+    description: blog.properties.Description.rich_text[0]?.plain_text || '',
+    slug: blog.properties.Slug.rich_text[0]?.plain_text || '',
     date: blog.properties.Date.date.start,
     tags: getTags(blog.properties.Tags.multi_select),
-    image: blog.properties.Image.files[0],
+    image: blog.properties.Image.files[0] || null,
   };
 }
+
+// * notion-to-mdの初期化
+const n2m = new NotionToMarkdown({ notionClient: notion });
 
 // @GET /api/blogs
 exports.getAllBlogs = async (req, res) => {
   console.log('@GET /api/blogs getAllBlogs');
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
-  });
-  const blogs = response.results;
-  const metadatas = blogs.map((blog) => {
-    return getPageMetaData(blog);
-  })
-  res.status(200).json(metadatas);
+  try {
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID,
+    });
+    const blogs = response.results;
+    const metadatas = blogs.map((blog) => {
+      return getPageMetaData(blog);
+    })
+    res.status(200).json(metadatas);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'error' });
+  }
 };
 
-// @GET /api/blogs/:blogId
-exports.getSingleBlog = (req, res) => {
-  console.log('@GET /api/blogs/:blogId getSingleBlog');
-  const blogId = req.params.blogId;
-  const blog = blogs.filter(blog => blog.id === parseInt(blogId));
-  res.status(200).json(blog);
+// @GET /api/blogs/:slug
+exports.getSingleBlog = async (req, res) => {
+  console.log('@GET /api/blogs/:slug getSingleBlog');
+  try {
+    const { slug } = req.params;
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID,
+      filter: {
+        property: 'Slug',
+        formula: {
+          string: {
+            equals: slug,
+          }
+        }
+      },
+    });
+
+    if (response.results.length === 0) {
+      return res.status(404).json({ error: '指定した記事が存在しません' });
+    }
+
+    const page = response.results[0];
+    const metadata = getPageMetaData(page);
+    const mdBlock = await n2m.pageToMarkdown(page.id);
+    const mdString = n2m.toMarkdownString(mdBlock);
+
+    res.status(200).json({
+      metadata: metadata,
+      markdown: mdString,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'error' });
+  }
 };
--- routes/blogs.router.js
+++ routes/blogs.router.js
@@ -5,7 +5,7 @@ const { getAllBlogs, getSingleBlog } = require('../controllers/blogs.controller'
 // @GET /api/blogs
 router.get('/', getAllBlogs);
 
-// @GET /api/blogs/blogId
-router.get('/:blogId', getSingleBlog);
+// @GET /api/blogs/slug
+router.get('/:slug', getSingleBlog);
 
 module.exports = router;
